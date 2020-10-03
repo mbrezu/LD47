8c3b6e592b01b68839c0fa300c7e923772dc0e7c
Miron Brezuleanu <mbrezu@gmail.com>
2020-10-03 17:11:06 +0300

    todos

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # LD47 submission
 
 This is the [Ludum Dare page](https://ldjam.com/events/ludum-dare/47/$223139).
+
+TODO:
+
+ * [ ] effects for disappearing tiles
+ * [ ] smooth movement for player
+ * [ ] player dies when touching map border
+ * [ ] (X1) player dies if a tile is not build in a given interval
+ * [ ] ui for (X1)
+ * [ ] show current path with arrows?
+ * [ ] particle effects for building tiles
+ * [ ] particle effects for disappearing tiles
+ * [ ] menus
+ * [ ] locked tiles
+ * [ ] superpowers (to be defined)
--- godot/scenes/game.gd
+++ godot/scenes/game.gd
@@ -15,16 +15,22 @@ func _process(_delta):
 #		player.advance()
 		Input.action_release("ui_select")
 		_add_tile = true
+		$player.advance()
 
 
-func _on_player_moved(old_row, old_column, row, column):
-	print("player moved ", [old_row, old_column, row, column])
+func _on_player_moved(old_row, old_column, _row, _column):
+#	print("player moved ", [old_row, old_column, row, column])
 	if _add_tile:
 		print("adding tile")
 		_add_tile = false
+		$advance_timer.wait_time = 1
+		$advance_timer.start()
 		$map.add_tile(old_row, old_column, $next_items.get_next_tile_number())
 		$next_items.advance()
 
 
 func _on_advance_timer_timeout():
 	$player.advance()
+	$advance_timer.wait_time -= 0.1
+	if $advance_timer.wait_time < 0.25:
+		$advance_timer.wait_time = 0.25
